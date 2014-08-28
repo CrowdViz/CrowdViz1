ff5aef7e7d9f71d2282126ab2f0d31ae0a1e8ee3
CrowdViz <aschramm@Andrews-MacBook-Pro-5.local>
2014-08-27 23:21:41 -0500

    CrowdViz
    Wireframes Draft

---
+++ CrowdViz1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CrowdViz1
+//
+//  Created by Andrew Schramm on 8/22/14.
+//  Copyright (c) 2014 CrowdViz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CrowdViz1Tests/CrowdViz1Tests.m
@@ -0,0 +1,34 @@
+//
+//  CrowdViz1Tests.m
+//  CrowdViz1Tests
+//
+//  Created by Andrew Schramm on 8/22/14.
+//  Copyright (c) 2014 CrowdViz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CrowdViz1Tests : XCTestCase
+
+@end
+
+@implementation CrowdViz1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
